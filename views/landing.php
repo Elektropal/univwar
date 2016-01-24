9e4dc234744c87a772be93e9f53e246ed5155e64
fawwazmuhammad <fawwazmuhammad@gmail.com>
2016-01-24 22:27:29 +0700

    views landing udah digabugn chat

--- views/landing.php
+++ views/landing.php
@@ -159,7 +159,7 @@
                         <div class="form-group col-md-12" style="text-align: left;">
                             <button type="submit" class="btn btn-lg btn-success"> Saya ikut #UniversityWar!</button>
                             <br/>
-	  					    <a href="/forgot/">Salah ketik memasukan email ? Klik disini !</a>
+	  					    <a href="#">Salah ketik memasukan email ? Ajukan komplain melalui chatbox di bawah !</a>
                         </div>
 	  				</form>
                     <hr>
@@ -314,6 +314,78 @@
 	
 	
 	<!-- ========== FOOTER SECTION ========== -->
+	<!-- ========== CHAT =================== -->
+	<div class="hidden-xs hidden-sm container chat">
+	    <div class="row chat-window col-xs-5 col-md-3" id="chat_window_1" style="margin-left:10px;">
+	        <div class="col-xs-12 col-md-12">
+	        	<div class="panel panel-default">
+	                <div class="panel-heading top-bar" id="paneltitle">
+	                    <div class="col-md-8 col-xs-8">
+	                        <h3 id="titel" class="panel-title"><span class="glyphicon glyphicon-comment"></span> Chat</h3>
+	                    </div>
+	                    <div class="col-md-4 col-xs-4" style="text-align: right;">
+	                        <a href="#"><span id="minim_chat_window" class="glyphicon glyphicon-minus icon_minim"></span></a>
+	                        <a href="#"><span class="glyphicon glyphicon-remove icon_close" data-id="chat_window_1"></span></a>
+	                    </div>
+	                </div>
+	                <div class="panel-body msg_container_base">
+	                	<!--
+	                    <div class="row msg_container base_sent">
+	                        <div class="col-md-10 col-xs-10">
+	                            <div class="messages msg_sent">
+	                                <p>that mongodb thing looks good, huh?
+	                                tiny master db, and huge document store</p>
+	                                <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+	                            </div>
+	                        </div>
+	                        <div class="col-md-2 col-xs-2 avatar">
+	                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">
+	                        </div>
+	                    </div>
+	                    <div class="row msg_container base_receive">
+	                        <div class="col-md-2 col-xs-2 avatar">
+	                            <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">
+	                        </div>
+	                        <div class="col-md-10 col-xs-10">
+	                            <div class="messages msg_receive">
+	                                <p>that mongodb thing looks good, huh?
+	                                tiny master db, and huge document store</p>
+	                                <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+	                            </div>
+	                        </div>
+	                    </div>
+	                	-->
+						
+						<div id="messagesDiv"></div>
+	                </div>
+	                <div class="panel-footer">
+	                    <div class="input-group">
+	                        <input id="messageInput" type="text" class="form-control input-sm chat_input" placeholder="Write your message here..." />
+	                        <span class="input-group-btn">
+	                        <button class="btn btn-primary btn-sm" id="btn-chat">Send</button>
+	                        </span>
+	                    </div>
+	                </div>
+	    		</div>
+	        </div>
+	    </div>
+	    
+	    <!--<div class="btn-group dropup">
+	        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+	            <span class="glyphicon glyphicon-cog"></span>
+	            <span class="sr-only">Toggle Dropdown</span>
+	        </button>
+	        <ul class="dropdown-menu" role="menu">
+	            <li><a href="#" id="new_chat"><span class="glyphicon glyphicon-plus"></span> Novo</a></li>
+	            <li><a href="#"><span class="glyphicon glyphicon-list"></span> Ver outras</a></li>
+	            <li><a href="#"><span class="glyphicon glyphicon-remove"></span> Fechar Tudo</a></li>
+	            <li class="divider"></li>
+	            <li><a href="#"><span class="glyphicon glyphicon-eye-close"></span> Invisivel</a></li>
+	        </ul>
+	    </div>
+		-->
+	</div>
+
 	<section id="contact" name="contact"></section>
 	<div id="f">
 		<div class="container">
@@ -396,6 +468,176 @@
 				<?php
 			}
 		?>
+	</script>
+	<script>
+	$(document).ready(function(){
+		$('#myfbsharer').click(function(){
+			FB.ui({
+			  method: 'share',
+			  href: '<?=Flight::get('my_base_url')?>/result',
+			}, function(response){});
+		});
+
+
+		function getCookie(cname) {
+		    var name = cname + "=";
+		    var ca = document.cookie.split(';');
+		    for(var i=0; i<ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0)==' ') c = c.substring(1);
+		        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		    }
+		    return "";
+		} 
+		
+		$(document).on('click', '.panel-heading span.icon_minim', function (e) {
+		    var $this = $(this);
+		    if (!$this.hasClass('panel-collapsed')) {
+		        $this.parents('.panel').find('.panel-body').slideUp();
+		        $this.addClass('panel-collapsed');
+		        $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
+		    } else {
+		        $this.parents('.panel').find('.panel-body').slideDown();
+		        $this.removeClass('panel-collapsed');
+		        $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
+		    }
+		});
+		$(document).on('focus', '.panel-footer input.chat_input', function (e) {
+		    var $this = $(this);
+		    /*
+		    if ($('#minim_chat_window').hasClass('panel-collapsed')) {
+		        $this.parents('.panel').find('.panel-body').slideDown();
+		        $('#minim_chat_window').removeClass('panel-collapsed');
+		        $('#minim_chat_window').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+		    }
+		    */
+		    $this.parents('.panel').find('.panel-body').slideDown();
+		    $this.removeClass('panel-collapsed');
+		    $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
+		});
+		$(document).on('click', '#new_chat', function (e) {
+		    var size = $( ".chat-window:last-child" ).css("margin-left");
+		     size_total = parseInt(size) + 400;
+		    alert(size_total);
+		    var clone = $( "#chat_window_1" ).clone().appendTo( ".container" );
+		    clone.css("margin-left", size_total);
+		});
+		$(document).on('click', '.icon_close', function (e) {
+		    //$(this).parent().parent().parent().parent().remove();
+		    $( "#chat_window_1" ).remove();
+		});
+
+		$(document).on('click', '#btn-chat', function (e) {
+		    console.log(getCookie('epal_chat'));
+		});
+		
+
+		// =========================== CHAT ==============================
+		var date = moment().format("YY/MM/DD");
+		var room = "<?php echo session_id();?>";//$('#roomInput').val();; //getCookie('epal_chat');
+		var myDataRef = new Firebase('https://android-chat-test.firebaseio.com/chat/'+room);
+		$('#messageInput').keypress(function (e) {
+			if (e.keyCode == 13) {
+				//var name = $('#nameInput').val();
+				name = "me";
+				var text = $('#messageInput').val();
+				var timestamp = Date.now();
+				myDataRef.push({username:name,content:text});//,room:room,timestamp:timestamp});
+				$('#messageInput').val('');
+			}
+		});
+		myDataRef.limitToLast(10).on('child_added', function(snapshot) {
+			var message = snapshot.val();
+			console.log(snapshot);
+			console.log("val()");
+			console.log(snapshot.val());
+			displayChatMessage(message.username, message.content);
+		});
+
+		function displayChatMessage(name, text) {
+			//$('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+			//$('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+			if(name=="me"){
+				$('#messagesDiv').append(
+					$("<div>")
+						.addClass("row msg_container base_receive")
+						.append(
+							$("<div>")
+								.addClass("col-md-2 col-xs-2 avatar")
+								.append(
+									$("<img>")
+										.addClass("img-responsive")
+										.attr("src","http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg")
+								)
+						).append(
+							$("<div>")
+								.addClass("col-md-10 col-xs-10")
+								.append(
+									$("<div>")
+										.addClass("messages msg_receive")
+										.append(
+											$("<p>").text(text)
+										).append(
+											$("<time>").text(name)
+										)
+								)
+						)
+				);
+			}else{				
+				$('#messagesDiv').append(
+					$("<div>")
+						.addClass("row msg_container base_sent")
+						.append(
+							$("<div>")
+								.addClass("col-md-10 col-xs-10")
+								.append(
+									$("<div>")
+										.addClass("messages msg_sent")
+										.append(
+											$("<p>").text(text)
+										).append(
+											$("<time>").text(name)
+										)
+								)
+						).append(
+							$("<div>")
+								.addClass("col-md-2 col-xs-2 avatar")
+								.append(
+									$("<img>")
+										.addClass("img-responsive")
+										.attr("src","http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg")
+								)
+						)
+				);
+			}
+			//$('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+			$("#messagesDiv").scrollTop($("#messagesDiv").scrollTop() + $("#messagesDiv").height()+20);
+			blink('#paneltitle',10,200);
+		};
+		$(window).unload(function(){
+			myDataRef.remove();
+		});
+
+		function blink(elem, times, speed) {
+		    if (times > 0 || times < 0) {
+		        if ($(elem).hasClass("blink")) 
+		            $(elem).removeClass("blink");
+		        else
+		            $(elem).addClass("blink");
+		    }
+
+		    clearTimeout(function () {
+		        blink(elem, times, speed);
+		    });
+
+		    if (times > 0 || times < 0) {
+		        setTimeout(function () {
+		            blink(elem, times, speed);
+		        }, speed);
+		        times -= .5;
+		    }		    
+		}
+	});
 	</script>
 </body>
 </html>
